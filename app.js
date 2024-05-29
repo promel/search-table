669eb0d5f95c5d7ff9623bd146f6d946c7782099
promel <jkm.more@gmail.com>
2024-05-29 22:34:54 +0200

    sort of working

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var app = angularmodule('myApp', ['ui.bootstrap']);
-app.filter('beginning_data', () => {
-    return (input, begin) => {
+var app = angular.module('myApp', ['ui.bootstrap']);
+app.filter('beginning_data', function() {
+    return function (input, begin) {
         if (input) {
             begin = +begin;
             return input.slice(begin);
@@ -9,24 +9,21 @@ app.filter('beginning_data', () => {
     }
 });
 
-app.controller('controller', ($scope, $http, $timeout) => {
-    $http.get('fetch-php').success((user_data) => {
-        $scope.file = user_data;
+app.controller('controller', function ($scope, $http, $timeout)  {
+    $http.get('https://www.hansafrique.co.za/Home/GetallCat').success(function (user_data){
+        $scope.file = user_data.result;
         $scope.current_grid = 1;
         $scope.data_limit = 10;
-        $scope.filter_data = $scope.file.Length;
-        $scope.entire_user = $scope.file.Length;
+        $scope.filter_data = $scope.file.length;
+        $scope.entire_user = $scope.file.length;
     });
-    $scope.page_position = (page_number) => {
+    $scope.page_position = function(page_number) {
+        console.log(page_number);
         $scope.current_grid = page_number;
     }
-    $scope.filter = () => {
-        $timeout(() => {
-            Sscopefilter_data = $scope.searched.Length;
+    $scope.filter = function (){
+        $timeout(function() {
+            Sscopefilter_data = $scope.searched.length;
         }, 20);
-    }
-    $scope.sort_with = (base) => {
-        $scope.base = base;
-        $scope.reverse = I$scope.reverse;
     }
 });
